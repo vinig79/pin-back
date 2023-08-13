4a6b6131a2b61e676c3f628cc918b8ffea3bb98a
Vinicius groto <viniciusgroto@gmail.com>
2023-08-13 18:55:36 -0300

    back_end

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import index from './src/routes/index.routes.js'
+import home from './src/routes/home.routes.js'
 
 const app = express();
 
 app.use('/',index);
+app.use('/home', home);
 
 const PORT = 5000;
 app.listen(PORT,()=>{
