bb1752f8eb95a97df2caf39685b9ebb36f90937b
Vinicius groto <viniciusgroto@gmail.com>
2023-08-13 18:43:07 -0300

    back_end

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import index from './src/routes/index.routes.js'
+
+const app = express();
+
+app.use('/index',index);
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+});
