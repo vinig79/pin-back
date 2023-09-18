35a56c511bf396ddddbdb0237c62b068762cc33e
Vinicius groto <viniciusgroto@gmail.com>
2023-09-17 22:06:54 -0300

    pg fix

--- src/database/config.js
+++ src/database/config.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
-import {postgress} from 'pg';
+import pg from 'pg';
 
 
 const sequelize = new Sequelize('wamyjrzi', 'wamyjrzi','eo-3voK8hX-12FJbtbP6I3kklhUp3rTr',
 {
     host:'silly.db.elephantsql.com',
-    dialect: postgress
+    dialect: pg
 }
 );
 
