6703906527add8e00b69789478be7d5cb44f33fb
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2023-09-19 14:38:07 -0300

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,12 +1,13 @@
 import { Sequelize } from "sequelize";
 import pg from 'pg';
 
-const sequelize = new Sequelize('wamyjrzi', 'wamyjrzi','eo-3voK8hX-12FJbtbP6I3kklhUp3rTr',
-{
-    host:'silly.db.elephantsql.com',
-    dialect: pg
-}
-);
+const sequelize = new Sequelize("postgres://default:0WHmhy3gIXRu@ep-jolly-grass-61658089-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+//('wamyjrzi', 'wamyjrzi','eo-3voK8hX-12FJbtbP6I3kklhUp3rTr',
+//{
+//    host:'silly.db.elephantsql.com',
+//    dialect: pg
+//}
+//);
 
 
 
