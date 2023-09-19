3f389839b6509ec064b6afcf1c48bdcfcff257b7
vinig79 <viniciusgroto@gmail.com>
2023-09-19 14:57:41 -0300

    deploy fix

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import pg from 'pg';
 
-const sequelize = new Sequelize("postgres://default:0WHmhy3gIXRu@ep-jolly-grass-61658089-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+const sequelize = new Sequelize("postgres://default:0WHmhy3gIXRu@ep-jolly-grass-61658089-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require")
 //('wamyjrzi', 'wamyjrzi','eo-3voK8hX-12FJbtbP6I3kklhUp3rTr',
 //{
 //    host:'silly.db.elephantsql.com',
