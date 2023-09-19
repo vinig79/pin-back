a99bc51f8122b36a22bef42be0d86a83a02d1b4a
Vinicius Groto de oliveira <102178039+vinig79@users.noreply.github.com>
2023-09-19 14:23:42 -0300

    Update config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
-
+import pg from 'pg';
 
 const sequelize = new Sequelize('wamyjrzi', 'wamyjrzi','eo-3voK8hX-12FJbtbP6I3kklhUp3rTr',
 {
     host:'silly.db.elephantsql.com',
-    dialect: 'postgres'
+    dialectModule: pg
 }
 );
 
