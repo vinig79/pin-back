68610d627d33cdc01ea0e7ea94e05069ce4a222a
Vinicius groto <viniciusgroto@gmail.com>
2023-09-19 21:06:05 -0300

    fix pg bug

--- src/database/config.js
+++ src/database/config.js
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize(
-    "postgres://default:0WHmhy3gIXRu@ep-jolly-grass-61658089-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require"
-    );
+const sequelize = new Sequelize("postgres://default:0WHmhy3gIXRu@ep-jolly-grass-61658089-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require");
 
 export default sequelize
