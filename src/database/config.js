ccb3a039e211064fb257051c0d17a4d1593defc6
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 07:21:45 -0300

    db

--- src/database/config.js
+++ src/database/config.js
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 import pg from 'pg'
 
 
-const sequelize = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb/verceldb?sslmode=require", {dialectModule: pg});
+const sequelize = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", {dialectModule: pg});
 
 export default sequelize
