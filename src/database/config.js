32bff6e4b6830f6edd0ce1c58ce99f4bcac3cf74
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 12:33:51 -0300

    fix

--- src/database/config.js
+++ src/database/config.js
@@ -2,6 +2,6 @@
 import pg from 'pg'
 
 
-const sq = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
+//const sq = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
 
-export default sequelize
+//export default sequelize
--- src/database/models.js
+++ src/database/models.js
@@ -1,5 +1,5 @@
 //import { Sequelize } from 'sequelize';
-import sequelize from "./config.js";
+//import sequelize from "./config.js";
 
 const User = sequelize.define('user', {
     id: {
@@ -57,4 +57,4 @@ Aulas.belongsTo(User, { foreignKey: "profId" });
     await Aulas.sync()
 })()
 
-export { User, Aulas }
+//export { User, Aulas }
