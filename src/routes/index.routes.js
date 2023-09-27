e727034239a4d47e7647d324f738703d45694e81
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 08:42:47 -0300

    db

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import path from 'path';
+import url from 'url';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const router = Router();
-const dir = __dirname
+
 
 router.get("/", (req, res) => {
-  res.json({server:dir});
+  res.json({server:__dirname});
 });
 
 export default router;
