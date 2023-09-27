358282f83522bc5ac116f011e5056cf48bd4385a
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 12:42:55 -0300

    fix

--- src/database/config.js
+++ src/database/config.js
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize/lib/index.js';
 import pg from 'pg'
 
 
--- src/database/models.js
+++ src/database/models.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize/lib/index.js";
 import sequelize from "./config.js";
 
 const User = sequelize.define('user', {
