1265fbc91273b3129b698d0beb8d343bc851d440
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 08:46:19 -0300

    db

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import {User} from "..//database/models.js"
+
 
 const router = Router();
 
 
 router.get("/", (req, res) => {
-  res.json({server:__dirname});
+  res.json({server:'on'});
 });
 
 export default router;
