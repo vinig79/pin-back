be59f6b969bba54446d0eae8f02bbc3ddb3e0a21
Vinicius groto <viniciusgroto@gmail.com>
2023-09-27 12:35:39 -0300

    fix

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,7 @@
-//import { Sequelize } from 'sequelize';
+const Sequelize = require('sequelize')
 import pg from 'pg'
 
 
-//const sq = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
+const sq = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
 
-//export default sequelize
+export default sequelize
