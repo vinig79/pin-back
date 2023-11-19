086205b3ea32a43f8df087f50ee7cee8445570bd
Vinicius groto <viniciusgroto@gmail.com>
2023-11-18 22:01:56 -0300

    database update

--- src/database/config.js
+++ src/database/config.js
@@ -2,6 +2,6 @@ import { Sequelize } from 'sequelize/lib/index';
 import pg from 'pg'
 
 
-const sequelize = new Sequelize("postgres://default:FuKjMI8D6ZWt@ep-blue-grass-56152755-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
+const sequelize = new Sequelize("postgres://default:2BAsbf7reutI@ep-black-hill-22594980-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
 
 export default sequelize
