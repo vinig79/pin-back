148745af77d232833c24afdd09a558b9fb79cafd
Vinicius groto <viniciusgroto@gmail.com>
2023-11-18 22:07:08 -0300

    database update

--- src/database/models.js
+++ src/database/models.js
@@ -1,83 +1,82 @@
 import { Sequelize } from "sequelize/lib/index";
 import sequelize from "./config.js";
 
-const User = sequelize.define(
-  "user",
-  {
+const User = sequelize.define('user', {
     id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
     nome: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false
     },
     email: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false
     },
     senha: {
-      type: Sequelize.STRING,
-      allowNull: false,
+        type: Sequelize.STRING,
+        allowNull: false
     },
     isprofessor: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-    },
-  },
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+},
 
-  {
-    tableName: "user",
-    timestamps: false,
-  }
-);
+    {
+        tableName: 'user',
+        timestamps: false
+    }
+)
 
-const Aulas = sequelize.define(
-  "aulas",
-  {
+const Aulas = sequelize.define('aulas', {
     id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
     url: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    tableName: "aula",
-    timestamps: false,
-  }
-);
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},
+    {
+        tableName: 'aula',
+        timestamps: false
+    }
 
-const Achievement = sequelize.define(
-  "achievement",
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+)
+
+
+const Achievement = sequelize.define("achievement", {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
-    achievements: {
-      type: Sequelize.INTEGER,
-    },
-  },
-  {
-    tableName: "aula",
-    timestamps: false,
-  }
-);
+    achievements:{
+        type: Sequelize.INTEGER
+    }
+},
+{
+    tableName: 'aula',
+    timestamps: false
+})
+
 
 User.hasMany(Aulas, { foreignKey: "profId", onDelete: "CASCADE " });
-User.belongsToMany(Achievement, { through: "UserAchievements" });
+User.belongsTo(Achievement, { through: 'UserAchievements'})
 
 Aulas.belongsTo(User, { foreignKey: "profId" });
 
-Achievement.belongsToMany(User, { through: "UserAchievements" })(async () => {
-  await User.sync();
-  await Aulas.sync();
-})();
+Achievement.belongsTo(User, { through: 'UserAchievements'})
 
-export { User, Aulas };
+
+(async () => {
+    await User.sync()
+    await Aulas.sync()
+})()
+
+export { User, Aulas }
