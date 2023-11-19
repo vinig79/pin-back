6ddadee046c1c6e0b5e18096931dfb595a22a8c3
Vinicius groto <viniciusgroto@gmail.com>
2023-11-19 13:45:46 -0300

    resgister router

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -5,20 +5,26 @@ const router = Router();
 
 router.post("/", async (req, res) => {
   const dado = req.body;
-  const user = await User.findOne({
-    where: {
-      email: dado.email,
-    },
-  });
-  if (user == null && dado != null) {
-    await User.create({
-      nome: dado.nome,
-      email: dado.email,
-      senha: dado.password1,
-      isprofessor: dado.isprofessor,
+  if (dado?.nome) {
+    const user = await User.findOne({
+      where: {
+        email: dado.email,
+      },
     });
+    if (user == null) {
+      await User.create({
+        nome: dado.nome,
+        email: dado.email,
+        senha: dado.password1,
+        isprofessor: dado.isprofessor,
+      });
+    } else {
+      console.log("passei");
+      res.json({ error: "usuario existente" });
+    }
   } else {
-    res.json({error})
+    console.log("passei");
+    res.json({ error: "json vazio" });
   }
   console.log(req.body);
   res.send(await User.findAll());
