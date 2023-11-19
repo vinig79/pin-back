ea522b0a903bac801796d721ed8e9473b05b2300
Vinicius groto <viniciusgroto@gmail.com>
2023-11-19 13:17:08 -0300

    resgister route

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -1,29 +1,27 @@
 import { Router } from "express";
 import { User } from "../database/models.js";
 
-
 const router = Router();
-
-
-
 
 router.post("/", async (req, res) => {
   const dado = req.body;
   const user = await User.findOne({
     where: {
-      email: dado.email
-    }
-  })
+      email: dado.email,
+    },
+  });
   if (user == null) {
     await User.create({
       nome: dado.nome,
       email: dado.email,
       senha: dado.password1,
-      isprofessor: dado.isprofessor
-    })
+      isprofessor: dado.isprofessor,
+    });
+  } else {
+    res.json({error})
   }
   console.log(req.body);
-  res.send(await User.findAll())
+  res.send(await User.findAll());
 });
 
 export default router;
