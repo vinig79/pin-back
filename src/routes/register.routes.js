d8990d03d0a5ef03d1b450195c072c45849e4b62
Vinicius groto <viniciusgroto@gmail.com>
2023-11-19 22:07:25 -0300

    register

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -4,31 +4,39 @@ import { User } from "../database/models.js";
 const router = Router();
 
 router.post("/", async (req, res) => {
-  if (!dado?.nome) {
-    console.log("passei");
-    return res.json({ error: "JSON vazio" });
-  }
-  
-  const user = await User.findOne({
-    where: {
-      email: dado.email,
-    },
-  });
-  
-  if (user == null) {
-    await User.create({
-      nome: dado.nome,
-      email: dado.email,
-      senha: dado.password1,
-      isprofessor: dado.isprofessor,
+  try {
+    const dado = req.body;
+
+    if (!dado?.nome) {
+      console.log("passei");
+      return res.json({ error: "JSON vazio" });
+    }
+
+    const user = await User.findOne({
+      where: {
+        email: dado.email,
+      },
     });
-    return res.send(await User.findAll());
-  } else {
-    console.log("passei");
-    return res.json({ error: "Email já cadastrado" });
+
+    if (user === null) {
+      await User.create({
+        nome: dado.nome,
+        email: dado.email,
+        senha: dado.password1,
+        isprofessor: dado.isprofessor,
+      });
+
+      // Obtenha e envie todos os usuários após a criação bem-sucedida
+      const users = await User.findAll();
+      return res.json(users);
+    } else {
+      console.log("passei");
+      return res.json({ error: "Email já cadastrado" });
+    }
+  } catch (error) {
+    console.error("Erro no processamento:", error);
+    return res.status(500).json({ error: "Erro interno no servidor" });
   }
-  
-  
 });
 
 export default router;
