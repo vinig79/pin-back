bc227348d52f55f458a824d395a3c2c313d2c125
vinig79 <viniciusgroto@gmail.com>
2023-11-21 21:33:00 -0300

    asa

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { User } from "../database/models.js";
-import jwt from "jsonwebtoken"; 
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  try {
+  try{
     const dado = req.body;
     if (!dado?.email || !dado?.password) {
       return res.json({ error: "JSON invalido" });
@@ -17,17 +16,18 @@ router.post("/", async (req, res) => {
       },
     });
 
-    if (user == null) {
-      return res.json({ error: "Usuario não existe" });
+    if (user == null){
+      return res.json({error: "Usuario não existe"})
     }
 
-    if (dado.password == user.senha) {
-      const token = jwt.sign({email:user.email}, "jwtSecretKey", {expiresIn: 300})
-      return res.json({ login: true, token});
-    } else {
-      return res.json({ error: "Senha incorreta" });
+    if (dado.password == user.senha){
+      return res.json({email:user.email, name: user.nome })
+    } else{
+      return res.json({error:"Senha incorreta"})
     }
-  } catch (error) {
+
+
+  }catch (error) {
     console.error("Erro no processamento:", error);
     return res.status(500).json({ error: "Erro interno no servidor" });
   }
