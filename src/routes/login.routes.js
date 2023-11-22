acda97bb0f5a3df68d5e3d01a106c1bbed8b9250
vinig79 <viniciusgroto@gmail.com>
2023-11-22 02:28:41 -0300

    f

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,39 +1,49 @@
-import { Router } from "express";
-import { User } from "../database/models.js";
-import jwt from "jsonwebtoken"; 
-
+import { Router } from 'express';
+import { User } from '../database/models.js';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const dado = req.body;
-    if (!dado?.email || !dado?.password) {
-      return res.json({ error: "JSON invalido" });
+    const { email, password } = req.body;
+
+    // Verifique se o JSON é inválido
+    if (!email || !password) {
+      return res.json({ error: 'JSON inválido' });
     }
 
+    // Busque o usuário no banco de dados
     const user = await User.findOne({
       where: {
-        email: dado.email,
+        email,
       },
     });
 
-    if (user == null) {
-      return res.json({ error: "Usuario não existe" });
+    // Se o usuário não existir, retorne um erro
+    if (!user) {
+      return res.json({ error: 'Usuário não existe' });
     }
 
-    if (dado.password == user.senha) {
-      console.log("to aqui")
-      const token = jwt.sign({email:user.email}, "jwt-secret-key", {expiresIn: 1000 * 60 * 60 * 24})
-      
-      res.cookie('token', token)
-      
-      return res.json({Status: "Sucess"})
-      
+    // Verifique a senha
+    if (password === user.senha) {
+      // Crie um token JWT
+      const token = jwt.sign({ email: user.email }, 'jwt-secret-key', {
+        expiresIn: '1d', // 1 dia de validade
+      });
+
+      // Configure o cookie com o token
+      res.cookie('token', token, { httpOnly: true });
+
+      // Retorne sucesso
+      return res.json({ Status: 'Sucesso' });
+    } else {
+      // Se a senha estiver incorreta, retorne um erro
+      return res.json({ error: 'Senha incorreta' });
     }
   } catch (error) {
-    console.error("Erro no processamento:", error);
-    return res.status(500).json({ error: "Erro interno no servidor" });
+    console.error('Erro no processamento:', error);
+    return res.status(500).json({ error: 'Erro interno no servidor' });
   }
 });
 
