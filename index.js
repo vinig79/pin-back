df6f590797e5c355705e048484b59f5cb22a00d6
vinig79 <viniciusgroto@gmail.com>
2023-11-21 21:31:11 -0300

    a

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-import express from "express";
+import express from 'express';
 //routes
-import index from "./src/routes/index.routes.js";
-import achievement from "./src/routes/achievement.routes.js";
-import register from "./src/routes/register.routes.js";
-import login from "./src/routes/login.routes.js";
-import session from "express-session";
-import cookieParser from "cookie-parser"
+import index from './src/routes/index.routes.js';
+import achievement from './src/routes/achievement.routes.js';
+import register from './src/routes/register.routes.js';
+import login from './src/routes/login.routes.js';
 
 //db
 //import {User, Aulas} from './src/database/models.js'
 
 //middleware
-import cors from "cors";
+import cors from 'cors';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser)
 
-app.use("/", index);
-app.use("/achievement", achievement);
-app.use("/register", register);
-app.use("/login", login);
+app.use('/', index);
+app.use('/achievement', achievement);
+app.use('/register', register);
+app.use('/login', login);
+
 
 const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
