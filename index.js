44a4d2e3e7d8bb18dd06e5a8af8dca29b89e0435
vinig79 <viniciusgroto@gmail.com>
2023-11-22 00:32:38 -0300

    index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import index from './src/routes/index.routes.js';
 import achievement from './src/routes/achievement.routes.js';
 import register from './src/routes/register.routes.js';
 import login from './src/routes/login.routes.js';
+import cookieParser from 'cookie-parser';
 
 //db
 //import {User, Aulas} from './src/database/models.js'
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser)
 
 app.use('/', index);
 app.use('/achievement', achievement);
