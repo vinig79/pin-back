16d10f3c59c419b4c7882be7dca7e7df8b67dc0c
vinig79 <viniciusgroto@gmail.com>
2023-11-21 21:29:31 -0300

    all

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,11 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  if(1 != 1){
-    return res.json({valid: true, email: req.session.userEmail, nome: req.session.userName})
-  } else{
-    return res.json({valid: false})
-  }
+  res.json({server:"on"});
 });
 
 export default router;
