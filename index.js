60bf3144fa80bf89f1d7776efd13a775bb02ca78
vinig79 <viniciusgroto@gmail.com>
2023-11-22 01:27:40 -0300

    index.js

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000"],
-    methods: ["POST","GET"],
-    credentials:true
+    origin: 'http://localhost:3000',
+    credentials: true, 
 }));
 app.use(cookieParser())
 
