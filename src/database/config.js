48ef2ab90b5d9a1d82bcd12ecac94d8ef3926e75
vinig79 <viniciusgroto@gmail.com>
2023-11-22 00:23:52 -0300

    db

--- src/database/config.js
+++ src/database/config.js
@@ -1,19 +1,8 @@
-import { Sequelize } from 'sequelize';
-import pg from 'pg';
+import { Sequelize } from 'sequelize/lib/index';
+import pg from 'pg'
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectModule: pg,
-  host: 'ep-floral-grass-11796545-pooler.us-east-1.postgres.vercel-storage.com',
-  port: 5432,
-  database: 'verceldb',
-  username: 'default',
-  password: 'nwMt5FXGQce9',
-  ssl: {
-    require: false,
-    rejectUnauthorized: false, // Configuração temporária para lidar com certificados autoassinados (ajuste conforme necessário)
-  },
-});
 
-export default sequelize;
+const sequelize = new Sequelize("postgres://default:nwMt5FXGQce9@ep-floral-grass-11796545-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require", { dialectModule: pg });
+
+
+export default sequelize
