1ab7eb6bb941b21a388fb94f4df7e280baa7f88c
vinig79 <viniciusgroto@gmail.com>
2023-11-21 21:55:11 -0300

    e

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import  jwt  from "jsonwebtoken";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({server:"on"});
+  const token = req.header["access-tonken"]
+  if(!token){
+    return res.json({error:"Token dont exists"})
+  } else{
+    jwt.verify(token, "jwtSecretKey", (err, decoded) =>{
+      if(err){
+        return res.json({error:"Not Authenticate"})
+      } else{
+        res.json({valid: true})
+      }
+    })
+  }
 });
 
 export default router;
