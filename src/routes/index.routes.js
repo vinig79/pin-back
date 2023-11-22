c2be4dbc0fc5613ddb458a2705c610221b8baf3f
vinig79 <viniciusgroto@gmail.com>
2023-11-21 21:22:57 -0300

    session

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({server:"on"});
+  if(req.session.userEmail){
+    return res.json({valid: true, email: req.session.userEmail, nome: req.session.userName})
+  } else{
+    return res.json({valid: false})
+  }
 });
 
 export default router;
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { User } from "../database/models.js";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  try{
+  try {
     const dado = req.body;
     if (!dado?.email || !dado?.password) {
       return res.json({ error: "JSON invalido" });
@@ -16,18 +17,17 @@ router.post("/", async (req, res) => {
       },
     });
 
-    if (user == null){
-      return res.json({error: "Usuario não existe"})
+    if (user == null) {
+      return res.json({ error: "Usuario não existe" });
     }
 
-    if (dado.password == user.senha){
-      return res.json({email:user.email, name: user.nome })
-    } else{
-      return res.json({error:"Senha incorreta"})
+    if (dado.password == user.senha) {
+      const token = jwt.sign({email:user.email}, "jwtSecretKey", {expiresIn: 300})
+      return res.json({ login: true, token});
+    } else {
+      return res.json({ error: "Senha incorreta" });
     }
-
-
-  }catch (error) {
+  } catch (error) {
     console.error("Erro no processamento:", error);
     return res.status(500).json({ error: "Erro interno no servidor" });
   }
