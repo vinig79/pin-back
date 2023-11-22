d7a8ee867194683c73796d72a0593d60587cf380
vinig79 <viniciusgroto@gmail.com>
2023-11-21 22:33:56 -0300

    lo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-
+import verifyUser from "../middleware/verifyUser.middleware.js";
 const router = Router();
 
-router.get("/" ,(req, res) => {
+router.get("/",verifyUser ,(req, res) => {
   return res.json({valid: true})
 });
 
